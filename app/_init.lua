4557bf4e1b96df92d9e27d8dd8ac0d9f48816e0a
Tobias Kieslich <tobias@evilroot.net>
2009-10-09 14:21:24 -0700

    Extend the lua based test output to compare why  the static delivery for files bigger than about 2K is failing in ab while it is working for single deliveries to the browser

--- app/_init.lua
+++ app/_init.lua
@@ -6,17 +6,48 @@ local result = [[<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 <body>
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
-  <b>I am a line</b>: Amazing isn\'t it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
+  <b>I am a line</b>: Amazing isn't it totally blowing your mind! ?! <br />
 </body>
 </html>]]
 
