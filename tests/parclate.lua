d72ed194b9a3e6ab03401e8918334ad0f89c6d9d
Tobias Kieslich <tobias@evilroot.net>
2009-12-01 00:45:16 -0800

    Start to write some tests. This is or Lua code
    	- test the Parclate class, make sure the parser runs with no errors

---
+++ tests/parclate.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env lua
+
+-- read a template
+
+package.path = '../?.lua;'..package.path
+
+local Parclate = require('parcle.Parclate')
+
+local template = [[
+<html att="blah_arg" xml:lang="en" lang="en" >
+	<head>
+		<title>A Lua-generated dynamic page</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<script type="text/javascript" language="JavaScript1.5">
+			var long = 123;
+			if (long%15 &gt; 7) {
+				alert("long is too long!");
+			}
+		</script>
+	</head>
+	<body>
+		<b>I am the <i>first</i> line</b>: Amazing isn't it! <br/>
+		<b>I am the <i>second</i> line</b>: Amazing isn't it! <br/>
+	</body>
+</html>
+]]
+
+
+local t = Parclate:new(template)  -- gen tmpl representation from xml string
+
