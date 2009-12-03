d2fdcb5b8f4dffb0f2a3c4b2ce227c7c4903f582
Tobias Kieslich <tobias@evilroot.net>
2009-12-03 01:54:49 -0800

    Parclate is not capturing leading or trailing whitespace in templates
    	- stuff before the first or past the last tag is simply omitted

--- tests/parclate.lua
+++ tests/parclate.lua
@@ -6,8 +6,7 @@ package.path = '../?.lua;'..package.path
 
 local Parclate = require('parcle.Parclate')
 
-local template = [[
-<html att="blah_arg" xml:lang="en" lang="en" >
+local template = [[<html att="blah_arg" xml:lang="en" lang="en" >
 	<head>
 		<title>A Lua-generated dynamic page</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -22,8 +21,7 @@ local template = [[
 		<b>I am the <i>first</i> line</b>: Amazing isn't it! <br/>
 		<b>I am the <i>second</i> line</b>: Amazing isn't it! <br/>
 	</body>
-</html>
-]]
+</html>]]
 
 
 local t = Parclate:new(template)  -- gen tmpl representation from xml string
