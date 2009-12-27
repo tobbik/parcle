8dcf885e5eed66b39711563796abf0d05a623b3a
Tobias Kieslich <tobias@evilroot.net>
2009-12-27 13:35:20 -0800

    Changing the index of the output array from lightuserdata to integer seems to reduce the number wrongly sent length request answers in the stress test, but it does not get rid of them

--- app/_init.lua
+++ app/_init.lua
@@ -102,14 +102,14 @@ local result = [[<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 </html>]]
 
 -- send the header information    Sun, 06 Nov 1994 08:49:37 GMT
-function test (cn)
+function test (cn, id)
 	--  buffer the entire output
-	output[cn] = 'HTTP/1.1 200 OK\r\n' ..
+	output[id] = 'HTTP/1.1 200 OK\r\n' ..
 		'Server: testserver/0.1\r\n' ..
 		'Content-Type: text/html\r\n' ..
 		'Last-Modified: ' .. os.date('%a, %d %b %Y %H:%M:%S') .. '\r\n' ..
 		'Content-Length: ' .. #result .. '\r\n\r\n' .. result
-	parcle.prepare(cn, output[cn])
+	parcle.prepare(cn, output[id])
 end
 
 
--- src/app.c
+++ src/app.c
@@ -87,7 +87,8 @@ void
 		/* Execute the lua function we want */
 		lua_getglobal(L, "test");
 		lua_pushlightuserdata(L, (void*) cn);
-		lua_call(L, 1, 0);
+		lua_pushnumber(L, cn->id);
+		lua_call(L, 2, 0);
 
 		/* signal the select loop that we are done ...*/
 		snprintf (answer_buf, 6, "%d", cn->id);
